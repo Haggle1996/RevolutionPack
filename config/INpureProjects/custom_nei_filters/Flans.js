0a6cebd7af50b270db44aa00ca60a0a7ae5de8a6
Haggle1996 <haggle1996@gmail.com>
2015-12-22 21:06:13 -0700

    Adding Wholly guns

--- config/INpureProjects/custom_nei_filters/Flans.js
+++
@@ -1,6 +0,0 @@
-if (FML.isModLoaded("flansmod")) {
-	NEI.hide("flansmod:teamsSpawner");
-	NEI.override("flansmod:flansWorkbench",[1]);
-	NEI.hide("flansmod:opStick");
-	NEI.hide("flansmod:flagpole");
-}
---
+++ config/INpureProjects/custom_nei_filters/GunCus.js
@@ -0,0 +1,8 @@
+if (FML.isModLoaded("GunCus")) {
+    //NEI.override("GunCus:item.Official.m16a3",[0]);
+    //NEI.override("GunCus:item.Official.aek971", [0]);
+    //NEI.override("GunCus:item.Official.l96", [0]);
+    //NEI.override("GunCus:item.Official.sv98", [0]);
+    //NEI.override("GunCus:item.Official.g17",[0]);
+    //NEI.override("GunCus:item.Official.g18",[0]);
+}
